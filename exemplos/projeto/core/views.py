4b75bf8376cc5f0ea238c14e5063f718e8b7802c
eoliveiradeliz <eoliveiradeliz@gmail.com>
2023-07-06 15:22:58 -0300

    turtle python

--- exemplos/projeto/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Pessoa
-
-# Create your views here.
-
-
-def home(request):
-    pessoas = Pessoa.objects.all()
-    return render(request, "index.html",("pessoas:", pessoas) )
---
+++ exemplos/turtle/turtle1.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+import turtle
+
+
+ws = turtle.Screen()
+
+ws.bgcolor("cyan")
+turtle.done()
